efc8f04cad3a1df52a1e3b1a4893a406c6d29bee
Valeria Marin <valeriamarin193@gmail.com>
2022-06-14 10:59:12 -0500

    Added tokenize function

---
+++ main.py
@@ -0,0 +1,38 @@
+# Read JSON as string
+
+json_str = """
+{
+    "k1": "value \"hey\" you",
+    "k2": -10.57,
+    "k3": {},
+    "k4": {"k1": null},
+    "k5": [1, "hello"]
+}
+"""
+
+
+def isString(char):
+    # Implementar búsqueda de strings
+    pass
+
+
+def tokenize(s: str) -> list:
+    tokens = []
+    while char < len(json_str):
+        # dict
+        if char == "{":
+            tokens.append = "OPEN_DICT"
+
+        # str
+        if isString(char):
+            tokens.append = "STRING"
+
+        # colon
+        if isString():
+            tokens.append = "STRING"
+
+        # comma
+
+        # int / float
+
+        # arr
