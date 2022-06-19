99d02d6957c9906f028253b651dbd2219eab3572
Valeria Marin <valeriamarin193@gmail.com>
2022-06-19 13:41:58 -0500

    test string

--- test.py
+++ test.py
@@ -1,4 +1,14 @@
-a = "value \"hey\" you"
-for i in a:
-    print(i)
-print('len: ', len(a))
+# a = "value \"hey\" you"
+# for i in a:
+#     print(i)
+# print('len: ', len(a))
+
+s = """
+{
+    "k1": "value \"hey\" you",
+    "k2": -10.57,
+    "k3": {},
+    "k4": {"k1": null},
+    "k5": [1, "hello"]
+}
+"""
